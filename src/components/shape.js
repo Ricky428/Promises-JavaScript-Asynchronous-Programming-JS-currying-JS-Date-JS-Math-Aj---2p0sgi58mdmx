f5b095f882781a4846b3f5705618d8f4899bf137
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-07-19 13:30:12 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
+class Shape {
+    constructor(shape) {
+        this.length=length;
+        this.breadth=breadth;
+        this.style.color='red';
+    }
+
+    drawShape(shape) {
+        this.style.color
+    }
+
+    calculateArea(length,breadth) {
+        return this.length * this.breadth;
+    }
+}
 
 // export class using module.exports
+module.exports = Shape; 
